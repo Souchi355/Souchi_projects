b13e6fbc9e33e141e74ce4fc5d1621956cc8230e
Souchi355 <90983402+Souchi355@users.noreply.github.com>
2021-09-18 19:46:43 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+n1 = int(input("number1:"))
+m = (input("marke:"))
+n2 = int(input("number2:"))
+
+if m == "+":
+	print(n1 + n2)
+elif m == "×":
+	print(n1 * n2)
+elif m == "÷":
+	print(n1 / n2)
+elif m == "-":
+	print(n1 - n2)
+elif m =="^":
+	print(n1 ** n2)
